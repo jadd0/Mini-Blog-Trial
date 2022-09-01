aa1678d82763d1f23668998879dd7c872bf6153d
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-01 22:22:37 +0100

    all authentication for /protected is now contained in own endpoint

--- src/routes/protected/+page.server.js
+++ src/routes/protected/+page.server.js
@@ -1,6 +1,8 @@
 import { parseCookie } from "../../cookieParser.js";
 
 import { Login } from "../../classes/login.js";
+import { error, redirect } from "@sveltejs/kit";
+
 const login = new Login();
 
 /** @type {import('./$types').Load} */
@@ -8,9 +10,20 @@ export function load({ request }) {
   const cookie = request.headers.get("cookie");
   const cookieList = parseCookie(cookie);
 
-  const jwt = JSON.parse(cookieList.jwt) || ''
+  if (cookieList.jwt == undefined) {
+    // throw error(401, 'show yourself, coward');
+    throw redirect(307, "/protected/login");
+  }
+
+  const jwt = JSON.parse(cookieList.jwt)
   
+  console.log(jwt)
+
   const user = login.authenticate(jwt.username, jwt.password)
+
+  if (!user) {
+    throw redirect(307, '/protected/login');
+  }
 
   console.log(user)
   return {
