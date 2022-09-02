f7af5a6c755db5f4419f9ce2e4ebcf50ef0b95be
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-02 15:43:30 +0100

    made createPost endpoint use dynamic values

--- src/routes/api/createPost/+server.js
+++ src/routes/api/createPost/+server.js
@@ -4,12 +4,10 @@ export const POST = async (event) => {
 	const userData = await event.json()
 	const { data, error } = await supabase.from("Posts").insert([
 		{
-			title: "Hello",
-			body: "Poop",
+			title: userData.title,
+			body: userData.body,
 			metadata: {
-				description: "Puppy is slower than other, bigger animals.",
-				price: 5.95,
-				ages: [3, 6],
+				description: userData.description,
 			},
 		},
 	]);
