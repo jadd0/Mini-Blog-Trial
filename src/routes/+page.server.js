df98da10aa8f5253e9fdd3f314782cd20da10be8
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-03 10:19:20 +0100

    load endpoint to return data directly

---
+++ src/routes/+page.server.js
@@ -0,0 +1,9 @@
+import { supabase } from "../supabaseClient.js";
+
+export const load = async() => {
+	const { data, error } = await supabase.from("Posts").select("*");
+
+	return {
+    data: data
+  }
+};
