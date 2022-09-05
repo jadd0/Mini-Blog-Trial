6fca383d98f52b716ca23d8b20ad360fd2d33a54
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-05 20:16:31 +0100

    new files to do with signing up

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let username
+  let name
+  let password
+  let email
+  async function send() {
+    const response = await fetch("/api/signUp", {
+			method: "post",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: username,
+        name: name,
+				password: password,
+        email: email
+			}),
+		});
+  }
+</script>
+
+<input bind:value={email}>
+<input bind:value={password}>
+<input bind:value={name}>
+<input bind:value={username}>
+<button on:click={send}>Here</button>
