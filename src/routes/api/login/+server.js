ac80f6f09c54efcdee195cb2a99c95adfe824c88
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-05 21:33:08 +0100

    new login system implementing firebase

--- src/routes/api/login/+server.js
+++ src/routes/api/login/+server.js
@@ -1,6 +1,24 @@
 import { Login } from "../../../classes/login.js";
+import { supabase } from "../../../supabaseClient.js";
 
-const login = new Login();
+// const login = new Login();
+
+async function login(username, password) {
+	const { data, e } = await supabase
+		.from("Users")
+		.select("*")
+		.eq("username", username);
+
+	if (data.length == 0) {
+		return false
+	}
+
+	if (data[0].password !== password) {
+		return false
+	}
+
+	return true
+}
 
 /** @type {import('./__types/[id]').RequestHandler} */
 export const POST = async ({ request, error }) => {
@@ -8,33 +26,31 @@ export const POST = async ({ request, error }) => {
 
 	const username = body.username;
 	const password = body.password;
-  
-  let serverResponse = new Response
-	const res = login.authenticate(username, password);
-	// if successful returns cookie
 
-	if (!res) {
-    // serverResponse.status.set(406)
-		// return new Response({
-    //   status: 401
-    // });
-    return new Response('Invalid credentials', {status: 406})
+	const auth = await login(username, password)
+
+	if (!auth) {
+		return new Response('Invalid credentials', {status: 406})
 	}
 
+	// if (!res) {
+	// 	return new Response("Invalid credentials", { status: 406 });
+	// }
 
-	const cookie = login.generateCookie();
-	
-	serverResponse.headers.append('set-cookie', cookie)
-	console.log(serverResponse.headers)
-  
+	// const cookie = login.generateCookie();
+
+	// serverResponse.headers.append("set-cookie", cookie);
+	// console.log(serverResponse.headers);
+
 	// serverResponse.status = 303
-  // console.log(serverResponse.headers);
+	// console.log(serverResponse.headers);
 	// return new Response("Redirect", {
-  //   cookies: cookie,
+	//   cookies: cookie,
 	// 	status: 303,
 	// 	headers: { Location: "/protected" },
 	// });
 	// Response.headers.set('set-cookie', response)
 
-	return serverResponse;
+	
+	// return serverResponse;
 };
