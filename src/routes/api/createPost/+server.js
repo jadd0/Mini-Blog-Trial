f4c65ffa9feb5a03df14bb7d02ca574455e85cf6
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-06 17:54:32 +0100

    response from newpost endpoint returned to not confuse things

--- src/routes/api/createPost/+server.js
+++ src/routes/api/createPost/+server.js
@@ -1,6 +1,25 @@
-import { supabase } from '../../../supabaseClient.js'
+import { parseCookie } from "../../../cookieParser.js";
+import { Login } from "../../../classes/login.js";
+import { error, redirect } from "@sveltejs/kit";
+import { supabase } from "../../../supabaseClient.js";
+import { checkAuth } from "../../checkAuth.js";
 
-export const POST = async ({ request }) => {
+const loginClass = new Login();
+
+
+/** @type {import('./$types').Load} */
+export async function POST({ request }) {
+  const cookie = request.headers.get("cookie");
+
+	const auth = await checkAuth(parseCookie, loginClass, cookie, supabase)
+
+	if (!auth) {
+		throw redirect(307, "/login");
+	}	
+	
+	const jwtName = parseCookie(cookie).jwt
+	const username = jwtName.replaceAll('"','')
+
 	const userData = await request.json()
 	const { data, error } = await supabase.from("Posts").insert([
 		{
@@ -8,10 +27,14 @@ export const POST = async ({ request }) => {
 			body: userData.body,
 			metadata: {
 				description: userData.description,
-				username: userData.username
 			},
+			a: username
 		},
 	]);
 
-	return new Response("hello");
-};
+	if(error == undefined) {
+		return new Response({status: 200})
+	}
+
+	return new Response({status: 404})
+}
