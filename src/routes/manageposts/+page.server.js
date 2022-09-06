187f7c07dd1a1b15f36623889b002eb95fd72662
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-06 12:52:46 +0100

    added authentication to all protected routes

--- src/routes/manageposts/+page.server.js
+++ src/routes/manageposts/+page.server.js
@@ -1,10 +1,29 @@
-import { supabase } from "../supabaseClient.js";
+import { parseCookie } from "../../cookieParser.js";
+import { Login } from "../../classes/login.js";
+import { error, redirect } from "@sveltejs/kit";
+import { supabase } from "../../supabaseClient.js";
 
-export const load = async () => {
-	const { data, error } = await supabase.from("Posts").select("*")
-  .eq('uuid', );
+const loginClass = new Login();
+
+/** @type {import('./$types').Load} */
+export async function load({ request }) {
+	const cookie = request.headers.get("cookie");
+	const cookieList = parseCookie(cookie);
+
+	if (cookieList.jwt == undefined) {
+		throw redirect(307, "/newpost/login");
+	}
+
+	const jwt = JSON.parse(cookieList.jwt);
+	const user = loginClass.authenticate(supabase, jwt.username, jwt.password);
+
+	if (!user) {
+		throw redirect(307, "/newpost/login");
+	}
+
+	const { data, error } = await supabase.from("Posts").select("*").eq("uuid");
 
 	return {
 		data: data.reverse(),
 	};
-};
+}
