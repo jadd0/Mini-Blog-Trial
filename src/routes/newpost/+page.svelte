67e3bd1b003cb3add125ce8ff48cf7f6b9ceebba
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-06 21:25:56 +0100

    new post creator and changed broken import

--- src/routes/newpost/+page.svelte
+++ src/routes/newpost/+page.svelte
@@ -1,62 +1,100 @@
 <script>
-	let title;
-	let body;
-	let description;
-	let username;
-	let data = [];
+	// import Nav from "../../components/nav.svelte";
+	import { onMount } from "svelte";
+
+	let title = "";
+	let description = "";
+	let body = "";
+
+	// function wrongDetails() {
+	// 	const uId = document.getElementsByClassName("username")[0]
+	// 	const pId = document.getElementsByClassName("password")[0]
+
+	// 	uId.style.border = "2px solid red"
+	// 	pId.style.border = "2px solid red"
+
+	// 	uId.classList.add("hello")
+	// 	console.log(uId.classList)
+	// }
 
 	const submit = async () => {
-		const data = {
-			title: title,
-			body: body,
-			description: description,
-			username: username
-		};
-
 		const response = await fetch("/api/createPost", {
 			method: "post",
 			headers: {
 				Accept: "application/json",
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(data),
+			body: JSON.stringify({
+				title: title,
+				description: description,
+				body: body
+			}),
 		});
-		// console.log(await response.json());
+
+		console.log(response);
+
+		if (response.ok) {
+			window.location = "/newpost";
+		}
+
+		if (response.ok == false) {
+			wrong = true;
+		}
 	};
-	const submitTWO = async () => {
-		const res = await fetch("/api/getPost", {
-			method: "post",
-			headers: {
-				Accept: "application/json",
-				"Content-Type": "application/json",
-			},
-		});
-		data = await res.json();
-		console.log(data);
-	};
+
+	function changeHeight() {
+		let scrollHeight = document.getElementById("new").scrollHeight;
+		console.log(scrollHeight);
+		document.getElementById("new").style.height = scrollHeight + "px";
+		// $("#message-box").css('height', scroll_height + 'px');
+	}
 </script>
 
 <body>
-	<input bind:value={title} />
-	<input bind:value={description} />
-	<input bind:value={body} />
-	<input bind:value={username} />
+	<!-- <Nav /> -->
+	<div id="form">
+		<h1>New Post</h1>
 
-	<button on:click={submit}>SEND</button>
-	<button on:click={submitTWO}>GET</button>
+		<div id="inputHolder">
+			<textarea
+				name="text"
+				id="userInput"
+				maxlength="50"
+				placeholder="Title (0-50 chars)"
+				bind:value={title}
+				oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'
+			/>
+		</div>
+		<div id="inputHolder">
+			<textarea
+				name="text"
+				id="userInput"
+				maxlength="100"
+				placeholder="Title (0-100 chars)"
+				bind:value={description}
+				oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'
+			/>
+		</div>
+		<div id="inputHolder">
+			<textarea
+				name="text"
+				id="userInput"
+				maxlength="2000"
+				placeholder="Title (0-2000 chars)"
+				bind:value={body}
+				oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'
+			/>
+		</div>
+
+		<button on:click={submit} id="loginButton">Send</button>
+	</div>
 </body>
-
 <style>
-	@font-face {
-		font-family: "New-Inter";
-		src: url("/fonts/Inter.ttf");
-	}
-
 	body {
 		margin: 0;
 		padding: 0;
 		border: 0;
-		height: 1000vh;
+		height: auto;
 		width: 100vw;
 		background-color: #141414;
 	}
@@ -70,19 +108,92 @@
 		border: 0;
 		outline: 0;
 		box-sizing: border-box;
-		/* font-family: New-Inter; */
 		font-family: New-Inter;
-		/* font-weight: 500;
-
-		font-variation-settings: 'wght' 500; */
-
-		letter-spacing: -2px;
-
-		/* font-family: "fenix";
-		font-weight: 500; */
+		font-weight: 300;
 	}
 
-	p {
-		font-weight: 250 !important;
+	@media (max-width: 930px) {
+		#form {
+			width: 340px !important;
+		}
+
+		#inputHolder {
+			width: 90% !important;
+		}
+	}
+
+	#userInput {
+		width: 87.5%;
+		min-height: 36px;
+		margin-left: 19px;
+		margin-top: 4px;
+		background: #212121;
+		color: white;
+		text-align: left;
+	}
+
+	#inputHolder {
+		width: 75%;
+		min-height: 40px;
+		margin: 0 auto;
+		margin-top: 10px;
+		background: #212121;
+		border-radius: 20px;
+		color: white;
+		text-align: left;
+	}
+
+	textarea {
+		resize: none;
+		width: 400px;
+		min-height: 100px;
+		height: auto;
+		padding: 5px;
+		overflow: hidden;
+		box-sizing: border-box;
+	}
+
+	h1 {
+		font-size: 1.75em;
+		font-weight: 800;
+		padding-top: 20px;
+		padding-bottom: 20px;
+		color: white;
+	}
+
+	#form {
+		margin: 0 auto;
+		background: #1b1b1b;
+		width: 40vw;
+		min-height: 400px;
+		padding-bottom: 30px;
+		border-radius: 15px;
+		margin-top: 10vh;
+	}
+
+	input {
+		margin: 0 auto;
+		line-height: 16px;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	#loginButton {
+		margin: 0 auto;
+		margin-top: 30px;
+		width: 75%;
+		height: 50px;
+		color: white;
+		font-weight: 600;
+		background-color: #212121;
+		border-radius: 20px;
+		cursor: pointer;
+		transition: all 0.2s linear;
+	}
+
+	#loginButton:hover {
+		background: rgb(56, 56, 56);
 	}
 </style>
