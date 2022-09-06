4ccae62a955b317d4f3d80335276ee21a2a71e87
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-06 17:56:06 +0100

    all endpoints are now secure

--- src/routes/api/getPost/+server.js
+++ src/routes/api/getPost/+server.js
@@ -1,6 +1,22 @@
+import { parseCookie } from "../../../cookieParser.js";
+import { Login } from "../../../classes/login.js";
+import { error, redirect } from "@sveltejs/kit";
 import { supabase } from "../../../supabaseClient.js";
+import { checkAuth } from "../../checkAuth.js";
 
-export const POST = async (event) => {
+const loginClass = new Login();
+
+
+/** @type {import('./$types').Load} */
+export async function POST({ request }) {
+  const cookie = request.headers.get("cookie");
+
+	const auth = await checkAuth(parseCookie, loginClass, cookie, supabase)
+
+	if (!auth) {
+		throw redirect(307, "/login");
+	}	
+	
 	const { data, error } = await supabase.from("Posts").select("*");
 
 	return new Response(JSON.stringify(data))
--- src/routes/api/getUsers/+server.js
+++ src/routes/api/getUsers/+server.js
@@ -1,6 +1,21 @@
+import { parseCookie } from "../../../cookieParser.js";
+import { Login } from "../../../classes/login.js";
+import { error, redirect } from "@sveltejs/kit";
 import { supabase } from "../../../supabaseClient.js";
+import { checkAuth } from "../../checkAuth.js";
 
+const loginClass = new Login();
+
+
+/** @type {import('./$types').Load} */
 export async function POST({ request }) {
+  const cookie = request.headers.get("cookie");
+
+	const auth = await checkAuth(parseCookie, loginClass, cookie, supabase)
+
+	if (!auth) {
+		throw redirect(307, "/login");
+	}	
 	const { data, error } = await supabase.from("Users").select("*");
 
   const req = await request.json()
