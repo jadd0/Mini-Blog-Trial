10ed5c14fbb3f8e0f398afdefe3aeaa7fb163bdb
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-06 14:21:17 +0100

    new page to get users

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,3 +1,136 @@
 <script>
-  
+	import { onMount } from "svelte";
+	// import Nav from './nav/+page.svelte'
+	let data = ''
+  let users = []
+
+  const postQuery = async () => {
+		const val = document.getElementById("userInput").value;
+
+		if (val == "") {
+			users = [];
+			return;
+		}
+
+		const rawResponse = await self.fetch(
+			"http://localhost:5173/api/getUsers",
+			{
+				method: "post",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ query: val }),
+			}
+		);
+		const h = await rawResponse.json();
+		users = h.users;
+
+    console.log(users)
+	};
+
+
 </script>
+
+<svelte:window on:keyup={postQuery} />
+
+<body>
+	<!-- <Nav/> -->
+	<div id="searchHolder">
+    <input id="userInput" type="text" />
+
+  </div>
+</body>
+
+<style>
+	@font-face {
+		font-family: "New-Inter";
+		src: url("/fonts/Inter.ttf");
+	}
+
+	body {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		height: 100vh;
+		width: 100vw;
+		background-color: #141414;
+		overflow-x: hidden
+	}
+
+	* {
+		font-size: 24px;
+		text-align: center;
+		color: black;
+		margin: 0;
+		padding: 0;
+		border: 0;
+		outline: 0;
+		box-sizing: border-box;
+		font-family: New-Inter;
+		letter-spacing: -1px !important;
+	}
+
+	#whole {
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		padding-bottom: 20px;
+	}
+
+	#postContainer {
+		width: 50vw;
+		/* min-height: 20vh; */
+		padding-bottom: 20px;
+		background: #212121;
+		margin-top: 20px;
+		border-radius: 10px;
+		transition: all 0.2s linear;
+	}	
+
+	#postContainer:hover {
+		background: rgb(56, 56, 56);
+	}
+
+	@media (max-width: 800px) {
+		#postContainer {
+			width: 75vw;
+		}
+	}
+
+	@media (max-width: 520px) {
+		h1 {
+			font-size: 10vw !important;
+		}
+	}
+
+	h1 {
+		font-size: 3rem;
+		font-weight: 700;
+		color: white;
+		padding-top: 10px;
+		text-align: left;
+		margin-left: 5vw;
+	}
+
+	h2 {
+		font-size: 1.5rem;
+		font-weight: 500;
+		color: white;
+		text-align: left;
+		/* margin-left: 1vw; */
+	}
+
+	#descriptionHolder {
+		width: 80%;
+		margin: 0 auto;
+		font-size: 1.5rem;
+		margin-left: 5vw !important;
+	}
+
+	a {
+		text-decoration: none;
+	}
+</style>
