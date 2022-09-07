328641083735890f4887ed37573c5a9749fd2d75
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-07 10:07:27 +0100

    reverse the post

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,7 +3,9 @@ import { supabase } from "../supabaseClient.js";
 export const load = async() => {
 	const { data, error } = await supabase.from("Posts").select("*");
   console.log(data)
+
+  
 	return {
-    data: data
+    data: data.reverse()
   }
 };
