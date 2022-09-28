8b52e2a71a61c18275aa38598469dc9422be0b8f
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-09-28 21:43:00 +0100

    new design for 'blog'

--- src/routes/newpost/components/__blog/+page.svelte
+++ src/routes/newpost/components/__blog/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-    let title;
-    let description;
-    let body;
+	import { autoresize } from 'svelte-textarea-autoresize'
+    let title = '';
+    let body = '';
 
     let loading = false
 
@@ -32,16 +32,26 @@
 	};
 </script>
 
-
-<div id="inputHolder">
+<div class="inputHolder">
+	<textarea
+	class="userInput"
+	maxlength="70"
+	placeholder="Title"
+	bind:value={title}
+	use:autoresize
+/>
+ <h3 id="forTitle">{title.length}/70</h3>
+</div>
+<div class="inputHolder" id="body">
   <textarea
-    name="text"
-    id="userInput"
-    maxlength="2000"
-    placeholder="Body (0-2000 chars)"
-    bind:value={body}
-    oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'
-  />
+	class="userInput"
+	maxlength="450"
+	placeholder="Body"
+	id="bodyInput"
+	bind:value={body}
+	use:autoresize
+/>
+ <h3 id="forBody">{body.length}/450</h3>
 </div>
 
 <button on:click={submit} id="loginButton">{loading === true ? "Loading..." : "Send"}</button>
@@ -61,72 +71,85 @@
 		font-weight: 300;
 	}
   @media (max-width: 930px) {
-		#form {
+		.form {
 			width: 340px !important;
 		}
 
-		#inputHolder {
+		.inputHolder {
 			width: 90% !important;
 		}
+
+		/* textarea {
+			font-size: 3.75vw !important;
+		}
+
+		h3 {
+					
+		} */
 	}
 
-	#userInput {
-		width: 87.5%;
-		min-height: 36px;
-		margin-left: 19px;
+	#body {
+		min-height: 100px;
+	}
+
+	#bodyInput {
+		min-height: 90px;
+	}
+
+	#forBody {
+		/* bottom: -40px !important;		 */
+	}
+
+	h3 {
+		font-weight: 600;
+		color: rgb(112, 112, 112);
+		font-size: 10px;
+		height: 0px;
+		width: 30px;
+		position: relative;
+		/* top:0; */
+		left: 84%;
+		text-align: right;
+		bottom: 20px;
+
+		/* margin-top: 10%; */
+		/* position: sticky; */
+		/* bottom: 0px !important; */
+		/* margin-top: 7.5%; */
+	}
+
+	.userInput {
+		width: 79%;
+		min-height: 25px;
+		margin-left: 10px;
 		margin-top: 4px;
 		background: #212121;
 		color: white;
 		text-align: left;
 	}
 
-	#inputHolder {
+	.inputHolder {
 		width: 75%;
-		min-height: 40px;
+		min-height: 30px;
+		padding-bottom: 5px;
 		margin: 0 auto;
 		margin-top: 10px;
 		background: #212121;
-		border-radius: 20px;
+		border-radius: 10px;
 		color: white;
 		text-align: left;
+		/* display: flex; */
 	}
 
 	textarea {
 		resize: none;
 		width: 400px;
 		min-height: 100px;
+		font-size: 16px;
 		height: auto;
 		padding: 5px;
 		overflow: hidden;
 		box-sizing: border-box;
-	}
-
-	h1 {
-		font-size: 1.75em;
-		font-weight: 800;
-		padding-top: 20px;
-		padding-bottom: 20px;
-		color: white;
-	}
-
-	#form {
-		margin: 0 auto;
-		background: #1b1b1b;
-		width: 40vw;
-		min-height: 400px;
-		padding-bottom: 30px;
-		margin-bottom: 30px;	
-		border-radius: 15px;
-		margin-top: 5vh;
-	}
-
-	input {
-		margin: 0 auto;
-		line-height: 16px;
-	}
-
-	a {
-		text-decoration: none;
 	}
 
 	#loginButton {
