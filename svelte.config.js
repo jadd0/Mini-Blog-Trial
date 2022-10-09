8b50fc993808a5ab45de74d5d3f926f7fa2fc5ab
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-10-09 12:51:14 +0100

    changed manifest accessability

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,24 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	files: {
+      assets: 'static',
+      hooks: {
+        client: 'src/hooks.client',
+        server: 'src/hooks.server'
+      },
+      lib: 'src/lib',
+      params: 'src/params',
+      routes: 'src/routes',
+      serviceWorker: 'src/service-worker.ts',
+      appTemplate: 'src/app.html',
+      errorTemplate: 'src/+error.svelte'
+    },
+		serviceWorker: {
+      register: true,
+      files: (filepath) => !/\.DS_Store/.test(filepath)
+    },
 };
 
 export default config;
