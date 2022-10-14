1387427b148b929c1d91c553478ffd4f7a85b2f1
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-10-14 20:39:31 +0100

    fixed resetpassword form

--- src/routes/resetpassword/[code]/+page.svelte
+++ src/routes/resetpassword/[code]/+page.svelte
@@ -1,15 +1,19 @@
 <script>
 	export let data;
-	const code = data.code
-	const passwordUsername = data.passwordUsername
 	import { onMount } from "svelte";
-	import Nav from '../../__nav/+page.svelte'
+	import Nav from "../../__nav/+page.svelte";
 
-	let password = ''
-	let password1 = ''
+	const username = data.passwordUsername
+	const code = data.code
+	let password = "";
+	let password1 = "";
+	let loading = false;
 
 	async function submit() {
-		if (password1 !== password) return
+		if (password != password1) return
+		loading = true
+		
+
 		const response = await fetch("/api/changePassword", {
 			method: "post",
 			headers: {
@@ -17,33 +21,36 @@
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify({
-				username: passwordUsername,
+				username,
 				code,
 				password
 			}),
 		});
-	}
-
-	function checkSame() {
-		if (password1 !== password) {
-			console.log("different")
-			return
-		}
+		console.log("hello")
+		if (response.ok ) location.href = '/login'
 	}
 </script>
 
-<svelte:window on:keyup={checkSame} />
+<!-- <svelte:window on:keyup={submit} /> -->
 
 <svelte:head>
 	<title>Reset Password</title>
 </svelte:head>
 
 <body>
-	<Nav username={data.username}/>
+	<Nav username={data.username} />
 	<div id="loginForm">
-		<input type="text" bind:value={password}>
-		<input type="text" bind:value={password1}>
-		<button on:click={submit}>send</button>
+		<h1>Reset Password</h1>
+		<p></p>
+		<div class="inputHolder">
+			<input type="text" class="userInput" bind:value={password} required />
+			<span class="floatingLabel">Type in password</span>
+		</div>
+		<div class="inputHolder">
+			<input type="text" class="userInput" bind:value={password1}	 required />
+			<span class="floatingLabel">Retype in new password</span>
+		</div>
+		<button on:click={submit} id="loginButton">{loading === true ? "Loading..." : "Reset"}</button>
 	</div>
 </body>
 
@@ -72,9 +79,55 @@
 		font-weight: 300;
 	}
 
-	#forgot {
+	.inputHolder {
+		width: 78%;
+		height: 50px;
+		padding-bottom: 5px;
+		margin: 0 auto;
+		margin-top: 10px;
+		background: #212121;
+		border-radius: 5px;
+		color: white;
+		text-align: left;
+		border: 2px solid #333;
+		transition: all 0.2s linear;
+	}
+	.userInput {
+		width: 92.5%;
+		min-height: 35px;
+		margin-left: 10px;
+		margin-top: 10px;
+		background: #212121;
+		color: white;
+		text-align: left;
+	}
+	.inputHolder .userInput:focus {
+		border-color: blue;
+		border-width: medium medium 2px;
+	}
+	.inputHolder .floatingLabel {
+		position: relative;
+		pointer-events: none;
+		top: -41px;
+		left: 10px;
+		transition: 0.2s ease all;
 		font-size: 15px;
-		margin-top: 1.5vh;
+		color: #727272;
+	}
+	.inputHolder input:focus ~ .floatingLabel,
+	.inputHolder input:not(:focus):valid ~ .floatingLabel {
+		top: -57px;
+		left: 10px;
+		font-size: 10px;
+		opacity: 1;
+	}
+
+	.inputHolder:focus-within {
+		border: 2px solid rgb(90, 90, 90);
+	}
+
+	input {
+		font-size: 17px !important;
 	}
 
 	@media (max-width: 460px) {
@@ -106,15 +159,12 @@
 	}
 
 	p {
-		margin-top: 20px;
+		margin-top: -20px;
+		padding: 0px 20px 5px 20px;
 		color: rgb(255, 255, 255);
-		font-size: 30px;
+		font-size: 14px;
 		font-weight: 400;
 		transition: all 0.2s linear;
-	}
-
-	p:hover {
-		color: rgb(127, 127, 127);
 	}
 
 	#userInput {
@@ -140,7 +190,7 @@
 	}
 
 	h1 {
-		font-size: 1.75em;
+		font-size: 35px;
 		font-weight: 600;
 		padding-top: 20px;
 		padding-bottom: 20px;
@@ -151,15 +201,15 @@
 		margin: 0 auto;
 		background: #1b1b1b;
 		width: 400px;
-		height: 350px;
+		height: 375px;
 		border-radius: 15px;
 		margin-top: 7vh;
 	}
 
-	input {
+	/* input {
 		margin: 0 auto;
 		line-height: 16px;
-	}
+	} */
 
 	a {
 		text-decoration: none;
@@ -167,13 +217,13 @@
 
 	#loginButton {
 		margin: 0 auto;
-		margin-top: 10px;
-		width: 75%;
+		margin-top: 30px;
+		width: 78%; 
 		height: 50px;
 		color: white;
 		font-weight: 600;
 		background-color: #212121;
-		border-radius: 20px;
+		border-radius: 5px;
 		cursor: pointer;
 		transition: all 0.2s linear;
 	}
