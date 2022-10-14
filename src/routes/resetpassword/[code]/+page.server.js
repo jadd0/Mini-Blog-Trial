e872a27a04a4e261456f7862b1012b3b115bb1b9
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-10-14 20:39:17 +0100

    check to ensure expiry code valid

--- src/routes/resetpassword/[code]/+page.server.js
+++ src/routes/resetpassword/[code]/+page.server.js
@@ -6,10 +6,14 @@ import { error } from "@sveltejs/kit";
 const supabaseClass = new SupabaseFeatures(supabase);
 const features = new Features()
 
-async function checkKey(username, code) {
+async function checkKey(username, code, time) {
   const user = await supabaseClass.getUser(username)
-
   if (code === user.keys.passwordReset) {
+    if (time < new Date().getTime()) {
+      supabaseClass.changeResetKey(username, '')
+      throw error(404, 'Reset code invalid')
+    }
+
     return true
   }
   return false
@@ -18,8 +22,9 @@ async function checkKey(username, code) {
 export const load = async({ request, params }) => {
   const cookie = features.parseCookie(request.headers.get("cookie"));
   const username = params.code.split('.')[0]
+  const time = params.code.split('.')[1]
 
-  const res = await checkKey(username, params.code)
+  const res = await checkKey(username, params.code, time)
   if (!res) throw error(404, 'Reset code invalid')
 
 
