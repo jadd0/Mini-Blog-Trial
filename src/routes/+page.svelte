2b009892a7443eb099f3dab9581a5fd8bd950a12
jadd0 <102069567+jadd0@users.noreply.github.com>
2024-01-04 21:53:30 +0000

    light changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,38 @@
-<h1>hello</h1>
+<body>
+  <h1>hello</h1>
+</body>
+
+<style>
+ @font-face {
+		font-family: 'Jakarta';
+		src: url('/fonts/Jakarta.ttf');
+	}
+
+  @font-face {
+		font-family: 'Circular-normal';
+		src: url('/fonts/CircularStd-Medium.otf');
+	} 
+
+  body {
+		overflow-x: hidden;
+		margin: 0;
+		padding: 0;
+		border: 0;
+		width: 100vw;
+		background-color: #141414;
+		overflow-x: hidden;
+	}
+
+	* {
+		font-size: 24px;
+		text-align: center;
+		color: black;
+		margin: 0;
+		padding: 0;
+		border: 0;
+		outline: 0;
+		box-sizing: border-box;
+		font-family: Circular-normal;
+		letter-spacing: -0px !important;
+	}
+</style>
