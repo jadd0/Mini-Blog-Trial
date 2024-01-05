875e3a49debedae59110ee564cc1e54e6b261e5a
jadd0 <102069567+jadd0@users.noreply.github.com>
2024-01-05 14:44:07 +0000

    snap scolling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,35 @@
 <body>
-  <h1>hello</h1>
+	<div class="container">
+		<div class="content">
+			<div class="marquee">
+				<p>Marquee in CSS</p>
+			</div>
+			1
+		</div>
+		<div class="content">2</div>
+		<div class="content">3</div>
+		<div class="content">4</div>
+	</div>
 </body>
 
 <style>
- @font-face {
+	@font-face {
 		font-family: 'Jakarta';
 		src: url('/fonts/Jakarta.ttf');
 	}
 
-  @font-face {
+	@font-face {
 		font-family: 'Circular-normal';
 		src: url('/fonts/CircularStd-Medium.otf');
-	} 
+	}
 
-  body {
+	body {
 		overflow-x: hidden;
 		margin: 0;
 		padding: 0;
 		border: 0;
 		width: 100vw;
+		/* height: 1000vh; */
 		background-color: #141414;
 		overflow-x: hidden;
 	}
@@ -35,4 +46,89 @@
 		font-family: Circular-normal;
 		letter-spacing: -0px !important;
 	}
+
+	.container,
+	.content {
+		width: 100vw;
+		height: 100vh;
+		font-size: 60px;
+	}
+
+	.container {
+		overflow: auto;
+		-webkit-scroll-snap-type: y mandatory;
+		-ms-scroll-snap-type: y mandatory;
+		scroll-snap-type: y mandatory;
+	}
+
+	.content {
+		scroll-snap-align: start;
+	}
+
+	.container,
+	.content {
+		margin: 0 auto;
+	}
+
+	.content {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: white;
+	}
+
+	.marquee {
+                height: 50px;
+                overflow: hidden;
+                position: relative;
+                background: #fefefe;
+                color: #333;
+                border: 1px solid #4a4a4a;
+            }
+            
+            .marquee p {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                margin: 0;
+                line-height: 50px;
+                text-align: center;
+                -moz-transform: translateX(100%);
+                -webkit-transform: translateX(100%);
+                transform: translateX(100%);
+                -moz-animation: scroll-left 2s linear infinite;
+                -webkit-animation: scroll-left 2s linear infinite;
+                animation: scroll-left 20s linear infinite;
+            }
+            
+            @-moz-keyframes scroll-left {
+                0% {
+                    -moz-transform: translateX(100%);
+                }
+                100% {
+                    -moz-transform: translateX(-100%);
+                }
+            }
+            
+            @-webkit-keyframes scroll-left {
+                0% {
+                    -webkit-transform: translateX(100%);
+                }
+                100% {
+                    -webkit-transform: translateX(-100%);
+                }
+            }
+            
+            @keyframes scroll-left {
+                0% {
+                    -moz-transform: translateX(100%);
+                    -webkit-transform: translateX(100%);
+                    transform: translateX(100%);
+                }
+                100% {
+                    -moz-transform: translateX(-100%);
+                    -webkit-transform: translateX(-100%);
+                    transform: translateX(-100%);
+                }
+            }
 </style>
