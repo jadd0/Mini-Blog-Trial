e5f5a99848fcbbe62c9f7f062fbb94841e90e844
jadd0 <102069567+jadd0@users.noreply.github.com>
2024-01-12 16:50:54 +0000

    backend working now (hopefully)

--- src/api/updatePhone/+server.ts
+++ src/api/updatePhone/+server.ts
@@ -1,8 +1,49 @@
-const supabaseUrl = process.env.VITE_SUPABASE_URL || import.meta.env.VITE_SUPABASE_URL
+import { supabase } from '../../supabaseClient'
 
-export async function POST({ request }) {
+function generateRandomString() {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]!"£$%^&*()-+=#.';
+  let randomString = '';
 
-  console.log(request)
+  for (let i = 0; i < 16; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
 
-  return new Response('Success')
+  return randomString;
+}
+
+async function getKey(key: string): Promise<any> {
+  const { data, error } = await supabase
+    .from('Keys')
+    .select('type')
+    .match('updatePhone');
+
+  if (error) return false
+
+  if (key != data.key) return false
+  return true
+}
+
+async function setKey() {
+  const { data, error } = await supabase
+    .from('Keys')
+    .update({ 'key': generateRandomString() })
+    .match({ 'type': 'phoneUpdate' })
+    .select()
+		
+  if (error) return false
+  return data.key
+}
+
+export async function POST({ request, url, error  }) {
+  const key = url.searchParams.get('key')
+  const location = url.searchParams.get('location')
+
+  const constKey = await getKey(key)
+  if (!getKey(constKey)) throw error(404, 'Bad key');
+
+  const newKey = setKey()
+  if (!setKey()) throw error(500, 'Error changing key')
+
+  return new Response(JSON.stringify({key: newKey}))
 }
