5e1ef59205a9ef4ca9cb2fc41222936a2338d9c1
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-02-14 16:31:45 +0000

    small change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,54 +4,22 @@
 
 <SvelteInertiaScroll>
 	<body>
-    <div class="section">
-      <strong>Scroll down!</strong>
-    </div>
-    <div class="section">
-      <strong>2</strong>
-    </div>
-    <div class="section">
-      <strong>3</strong>
-    </div>
-    <div class="section">
-      <strong>4</strong>
-    </div>
-    <div class="section">
-      <strong>5</strong>
-    </div>
-  </body>
+		<div class="hero" />
+	</body>
 </SvelteInertiaScroll>
 
 <style>
+	body {
+		margin: 0;
+		padding: 0;
+		width: 100%;
+		background: #121212;
+    height: 10000vh;
+	}
 
-
-body {
-  margin: 0; 
-  padding: 0;
-  width: 100%;
-}
-
-.section {
-  box-sizing: border-box;
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-family: sans-serif;
-  font-size: 4em;
-  &:first-child {
-    background-color: #ccc;
+  .hero {
+    width: 100vw;
+    height: 100vh;
+    background: white;
   }
-  &:nth-child(2) {
-    color: #efefef;
-    background-color: #212121;
-  }
-  &:nth-child(3) {
-    background-color: #888;
-  }
-  &:nth-child(4) {
-    background-color: #f00;
-  }
-}
 </style>
