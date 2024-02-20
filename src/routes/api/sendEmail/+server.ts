9ec272438a780c35e8794ea73d45d69d03209c60
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-02-20 12:02:59 +0000

    endpoint for sending email

--- src/routes/api/sendEmail/+server.ts
+++ src/routes/api/sendEmail/+server.ts
@@ -1,35 +1,57 @@
-import { get } from 'svelte/store';
-import { auth, passwordReset } from '../../stores/objects';
+import nodemailer from 'nodemailer'
 
-const Auth = get(auth)
-const PasswordReset = get(passwordReset)
+async function sendEmail(email: string, name: string, enquiry: string) {
+		const user = process.env.VITE_NODEMAILER_EMAIL || import.meta.env.VITE_NODEMAILER_EMAIL;
+		const pass = process.env.PASSWORD || import.meta.env.VITE_NODEMAILER_PASSWORD;
+
+		let transport = nodemailer.createTransport({
+			host: 'smtp-relay.sendinblue.com',
+			port: 587,
+			auth: {
+				user,
+				pass
+			}
+		});
+
+		transport.verify(function (error, success) {
+			if (error) {
+				return 'auth';
+			}
+		});
+
+		const mailOptions = {
+			from: 'jaddlive@jaddblog.com',
+			to: 'jaddalkwork@gmail.com',
+			subject: 'New Contact!',
+			html: `<h1>From ${name}</h1>
+			<br/>
+			<p>Enquiry: ${enquiry}
+			<br/>
+			<br/>
+			Email: ${email}
+			<br/>
+			<br/></p>`
+		};
+
+		transport.sendMail(mailOptions, function (err, info) {
+			console.log(err, info)
+			if (err) {
+				return false;
+			}
+		});
+
+		return true;
+	}
 
 /** @type {import('./__types/[id]').RequestHandler} */
 export const POST = async ({ request }) => {
 	const body = await request.json();
-	const res = await Auth.checkEmail(body.username, body.email);
-	console.log(res)
-	if (!res) {
-		return new Response('Invalid email/username', { status: 401 });
-	}
 
-	// 15 minutes
-	const time = 900000;
-	const code = Auth.Parse.generateToken(40, time);
-	const date = new Date().getTime() + time;
-	const email = await PasswordReset.sendEmail(body.email, body.username, 'Reset Password', code, date);
-	console.log(email)
+	const email = await sendEmail(body.emaik, body.name, body.enquiry);
+
 	if (!email) {
 		return new Response(
 			'Sorry, there has been an issue sending the email. Please try again later. If this issue persists, please email me jaddalkwork@gmail.com',
-			{ status: 500 }
-		);
-	}
-
-	const resKey = await PasswordReset.changeResetKey(body.username, code);
-	if (!resKey) {
-		return new Response(
-			'Sorry, there has been an issue changing your reset key. Please try again later. If this issue persists, please email me jaddalkwork@gmail.com',
 			{ status: 500 }
 		);
 	}
