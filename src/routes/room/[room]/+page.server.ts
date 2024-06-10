3de09feef24f6690e0a0a443a98603cacb3a0e72
jadd0 <102069567+jadd0@users.noreply.github.com>
2024-06-10 16:39:59 +0100

    hopefully working page server for room

--- src/routes/room/[room]/+page.server.ts
+++ src/routes/room/[room]/+page.server.ts
@@ -1,15 +1,33 @@
 import { get } from 'svelte/store'
 import { error } from '@sveltejs/kit';
+import { v4 as uuidv4 } from 'uuid';
 
 import { rooms } from "../../stores";
 
 const Rooms = get(rooms);
 
-export const load: any = async ({ request, cookies, fetch, params }) => {
+
+export const load: any = async ({ request, cookies, params }) => {
   const room = Rooms.getRoom(params.room);
 
   if (!room) {
     return new Error(`Invalid room ${params.room}`);
+  }
+
+  const cookie = request.headers.get("cookie").get("uuid");
+
+  if (!cookie) {
+    const uuid = uuidv4();
+
+    cookies.set('uuid', uuid, {
+      path: '/',
+      HostOnly: false,
+      Secure: 'lax',
+      httpOnly: true,
+      SameSite: 'Strict'
+    });
+
+    room.addMember(uuid);
   }
 
   return {...room};
