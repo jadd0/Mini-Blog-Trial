d9b884b48ed54d48e940f8971e09f97aa1ecca12
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2024-07-09 23:51:07 +0100

    albums looks how I wanted it

--- src/routes/__components/selection/albums/+page.svelte
+++ src/routes/__components/selection/albums/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import Page from "../albums/+page.svelte";
-
 	let albumName = '';
 	let isName: number = 0;
 	let albums: {}[] = [];
-	let selectedAlbumName: number | null = null;
+	let selectedAlbumIndex: number | null = null;
 
 	async function submit(e: any) {
+    selectedAlbumIndex = null;
 		if (albumName.length < 1) {
 			isName = 0;
 			return;
@@ -23,8 +22,10 @@
 		console.log(albums);
 	}
 
-	async function sendAlbum(album: any) {
-		const response = await fetch('/api/sendalbum', {
+	async function sendAlbum(album: any, index: number) {
+    selectedAlbumIndex = index;
+
+		const response = await fetch('/api/sendAlbum', {
 			method: 'post',
 			headers: {
 				Accept: 'application/json',
@@ -35,8 +36,6 @@
 			})
 		});
 	}
-
-
 </script>
 
 <svelte:window on:keyup={submit} />
@@ -51,42 +50,44 @@
   <span class="floatingLabel">Title</span>
 </div>
 
-<div class="desc">
-  <div class="empty" />
-  <div class="titleHolder"><h4>TITLE</h4></div>
-  <div class="albumTitle"><h4>ALBUM</h4></div>
-</div>
+<div class="mainHolder">
+	<div class="desc">
+		<div class="empty" />
+		<div class="titleHolder"><h4>TITLE</h4></div>
+		<div class="albumTitle"><h4>RELEASED</h4></div>
+	</div>
 
-<div class="albumContainer">
-  {#each albums as album, i}
-    <div class="album {selectedAlbumName === i ? 'selected' : ''}" on:click={() => sendAlbum(album)}>
-      <img src={album.album.images[0].url} class="thumbnail" />
-      <div class="properties">
-        <h2>{album.name}</h2>
-        <div class="explicitAndArtist">
-          {#if album.explicit}
-            <div class="explicitHolder">
-              <div class="explicit">
-                <span class="explicitText">E</span>
-              </div>
-            </div>
-          {/if}
-          <h3 class="artist">
-            {#each album.artists as artist, j}
-              {artist.name}{j < album.artists.length - 1 ? ', ' : ''}
-            {/each}
-          </h3>
-        </div>
-      </div>
-      <div class="albumHolder">
-        <h3>{album.album.name}</h3>
-      </div>
-    </div>
-  {/each}
+	<div class="albumContainer">
+		{#each albums as album, i}
+			<div class="album {selectedAlbumIndex === i ? 'selected' : ''}" on:click={() => sendAlbum(album, i)}>
+				<img src={album.images[0].url} class="thumbnail" />
+				<div class="properties">
+					<h2>{album.name}</h2>
+					<div class="explicitAndArtist">
+						{#if album.explicit}
+							<div class="explicitHolder">
+								<div class="explicit">
+									<span class="explicitText">E</span>
+								</div>
+							</div>
+						{/if}
+						<h3 class="artist">
+							{#each album.artists as artist, j}
+								{artist.name}{j < album.artists.length - 1 ? ', ' : ''}
+							{/each}
+						</h3>
+					</div>
+				</div>
+				<div class="albumHolder">
+					<h3>{album.release_date}</h3>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-  @font-face {
+	@font-face {
 		font-family: 'Circular';
 		src: url('/fonts/CircularStd-Book.otf');
 	}
@@ -115,60 +116,11 @@
 		font-weight: 300;
 	}
 
-  .selected {
-    background: green !important;
-  }
-
-  .inputHolder {
-		width: 300px;
-		height: 50px;
-		padding-bottom: 5px;
-		margin: 0 auto;
-		margin-top: 10px;
-		background: #212121;
-		border-radius: 5px;
-		color: white;
-		text-align: left;
-		border: 2px solid #333;
-		transition: all 0.2s linear;
-	}
-	.userInput {
-		width: 92.5%;
-		min-height: 35px;
-		margin-left: 10px;
-		margin-top: 10px;
-		background: #212121;
-		color: white;
-		text-align: left;
-	}
-	.inputHolder .userInput:focus {
-		border-color: blue;
-		border-width: medium medium 2px;
-	}
-	.inputHolder .floatingLabel {
-		position: relative;
-		pointer-events: none;
-		top: -40px;
-		left: 10px;
-		transition: 0.2s ease all;
-		font-size: 15px;
-		color: #727272;
-	}
-	.inputHolder input:focus ~ .floatingLabel,
-	.inputHolder input:not(:focus):valid ~ .floatingLabel {
-		top: -57px;
-		left: 10px;
-		font-size: 10px;
-		opacity: 1;
+	.selected {
+		background: green !important;
 	}
 
-	.inputHolder:focus-within {
-		border: 2px solid rgb(90, 90, 90);
-	}
-
-	input {
-		font-size: 17px !important;
-	}
+	
 
 	.desc {
 		width: 700px;
@@ -347,4 +299,60 @@
 		margin-top: 1px;
 	}
 
+
+  .inputHolder {
+		width: 300px;
+		height: 50px;
+		padding-bottom: 5px;
+		margin: 0 auto;
+		margin-top: 10px;
+		background: #212121;
+		border-radius: 5px;
+		color: white;
+		text-align: left;
+		border: 2px solid #333;
+		transition: all 0.2s linear;
+	}
+	.userInput {
+		width: 92.5%;
+		min-height: 35px;
+		margin-left: 10px;
+		margin-top: 10px;
+		background: #212121;
+		color: white;
+		text-align: left;
+	}
+	.inputHolder .userInput:focus {
+		border-color: blue;
+		border-width: medium medium 2px;
+	}
+	.inputHolder .floatingLabel {
+		position: relative;
+		pointer-events: none;
+		top: -40px;
+		left: 10px;
+		transition: 0.2s ease all;
+		font-size: 15px;
+		color: #727272;
+	}
+	.inputHolder input:focus ~ .floatingLabel,
+	.inputHolder input:not(:focus):valid ~ .floatingLabel {
+		top: -57px;
+		left: 10px;
+		font-size: 10px;
+		opacity: 1;
+	}
+
+	.inputHolder:focus-within {
+		border: 2px solid rgb(90, 90, 90);
+	}
+
+	input {
+		font-size: 17px !important;
+	}
+
+
+	
+
+	
 </style>
